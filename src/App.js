8ce2f87657c294919f16b67bea357ec573b32f74
tymulamichal <michal.tymula@amsterdamstandard.com>
2022-08-18 12:57:13 +0200

    refactored App to functional component, changed export to named export

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import './index.css'
 import Nav from './Nav.js'
 import Main from './Main.js'
 
-class App extends React.Component {
-    render() {
-        return (
-         <div> 
-            <Nav />
-            <Main />
-         </div>
-        )
-    }
+export function App() {
+
+    return (
+        <div>
+            <Nav/>
+            <Main/>
+        </div>
+    )
 }
 
-export default App
+// export default App
